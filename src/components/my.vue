418d0d5b5e2ce2aca2c9c4acbeadfe5f1066225b
sun yue ying <609719104@qq.com>
2018-08-03 14:16:58 +0800

    孙月莹合并修改第一次

--- src/components/my.vue
+++ src/components/my.vue
@@ -29,39 +29,17 @@ ul{
 	height: 40px;
 	line-height: 40px;
 	width: 100%;
-	background: #000;
-	color: #fff;
+	background: #fff;
+	color: #2c3038;
 	position: fixed;
   z-index: 999;
 	bottom: 0;
 	left: 0;
+  box-shadow: 0 -1px 0 0 rgba(0,0,0,.1);
 	display: flex;
 	li{
 		flex : 1 ;
 		text-align: center
 	}
 }
-.bottom-bar{
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    text-align: center;
-    color: #2c3038;
-    font-size: 10px;
-    background-color: #fff;
-    box-shadow: 0 -1px 0 0 rgba(0,0,0,.1);
-    padding-top: 5px;
-    display: flex;
-    height: 44px;}
-    .bottom-bar li[data-v-2eea505b] {
-    width: 50%;
-    flex: 1;
-    list-style: none
-}
-  .bottom-bar li img{
-    display: block;
-    margin: 0 auto;
-    height: 22px;
-  }
 </style>
