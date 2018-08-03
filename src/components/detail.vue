10960e65235bf1aa55b0569984ab72bd2dd9169d
sun yue ying <609719104@qq.com>
2018-08-03 17:10:06 +0800

    啥也没做

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="detail">
-
-    <div id="banner">
-      <ul>
-        <li></li>
-      </ul>
+    <div id="banner" v-if="pro_img!=null&& pro_img.length>0">
+      <swipe class="my-swipe" v-for="img in pro_img">
+        <swipe-item class="slide1"">
+          <img :src="img.img_url">
+        </swipe-item>
+      </swipe>
     </div>
 
 
@@ -13,27 +14,55 @@
 </template>
 
 <script>
-  import router from "../router";
-  import axios from "axios"
+  // import router from "../router";
+  import axios from "axios";
+  require('vue-swipe/dist/vue-swipe.css');
+  import { Swipe, SwipeItem } from 'vue-swipe';
 export default {
   name: 'detail',
   data () {
     return {
-      product:[]
+      all:[],
+      basic:[],
+      pro_img:[]
     }
   },
   mounted(){
     //https://api.ricebook.com/product/info/product_detail.json?product_id=1046843
   	console.log(this.$route.params.id);
     axios.get(`/product/info/product_detail.json?product_id=${this.$route.params.id}`).then(res=>{
-      console.log(res.data)
-      this.product = res.data
+      // console.log(res.data)
+      this.all = res.data;
+
+      // console.log(res.data.basic)
+      this.basic = res.data.basic
+      // console.log(this.basic);
+
+      this.pro_img = res.data.basic.product_images;
+      console.log(this.pro_img)
+
     })
+  },
+  components:{
+    Swipe,
+    SwipeItem
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .my-swipe {
+    height: 200px;
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+    background: pink;
+  }
+
+  .slide1 {
+    background-color: #0089dc;
+    color: #fff;
+  }
 
 </style>
